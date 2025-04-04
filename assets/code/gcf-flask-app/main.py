a00702bd2321258676dac013ee4430f488b22859
Matteo Martinelli <wmatte2005@gmail.com>
2025-04-04 11:54:36 +0200

    Modified the exercise to solve errors
    The escape module of Flask was impossible to import.
    Since it was not used, it was deleted.

--- assets/code/gcf-flask-app/main.py
+++ assets/code/gcf-flask-app/main.py
@@ -1,16 +1,14 @@
-from flask import escape
+def hello_http(request):
+    """
+    HTTP Cloud Function.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
 
-    def hello_http(request):
-        """
-        HTTP Cloud Function.
-        """
-        request_json = request.get_json(silent=True)
-        request_args = request.args
-
-        if request_json and 'name' in request_json:
-            name = request_json['name']
-        elif request_args and 'name' in request_args:
-            name = request_args['name']
-        else:
-            name = 'World'
-        return f'Hello {escape(name)}!'
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return f'Hello {(name)}!'
